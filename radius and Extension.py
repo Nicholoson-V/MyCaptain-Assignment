809f90ff094d055577202068f89096d489b512b3
Nicholoson-V <109790233+Nicholoson-V@users.noreply.github.com>
2022-07-25 15:44:06 +0530

    Add files via upload

---
+++ radius and Extension.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 25 10:14:21 2022
+
+@author: LENOVO
+"""
+#Task 1
+
+radius=float(input("Enter the radius of a circle:"))
+print("The area of a circle is:" +str(3.14*radius*radius))
+
+
+**************************
+
+#Task 2
+
+fileName=input("Enter the fileName:")
+fileExtension = fileName.split(".")
+print("The file Extension is:" + fileExtension[-1])
