a4b669ab5b8117a65809dc38eb48f68f918992fa
Nicholoson-V <109790233+Nicholoson-V@users.noreply.github.com>
2022-08-03 16:33:09 +0530

    Add files via upload

---
+++ positive num.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  3 16:26:57 2022
+
+@author: NICK
+"""
+list = [5,0,-1,25,-10]
+
+for number in list:
+    
+    if number >= 0:
+        print (number)
